de093ce5fcfe2e73d439a47404c0c923269536c7
Gage-McPursifull <59587881+Gage-McPursifull@users.noreply.github.com>
2021-05-24 22:22:18 -0700

    Made many changes to all three files

--- pie_chart.py
+++ pie_chart.py
@@ -1,71 +1,150 @@
-import matplotlib.pyplot as plt
+# Gage McPursifull
+# CS 361
+# Creates a socket connection to gaia.cs.umass.edu, and sends a GET request to
+# /wireshark-labs/HTTP-wireshark-file3.html. This program differs from small_get.py, in that it can handle a GET request
+# of any size.
+# Sources: Textbook - K&R: Chapter 2.7 TCPServer.py
+# https://zetcode.com/python/socket/ - Echo client server example
 
-info_to_parse = ("Headquarters near Beaverton, Oregon|FormerlyBlue Ribbon Sports, Inc.(1964–1971)|TypePublic|Traded "
-                 "asNYSE: NKE (Class B)DJIA componentS&P 100 componentS&P 500 component|"
-                 "IndustryApparelAccessoriesSports equipment|FoundedJanuary 25, 1964; 57 years ago (1964-01-25)|"
-                 "FoundersBill BowermanPhil Knight|HeadquartersBeaverton, Oregon, U.S.|Area servedWorldwide|"
-                 "Key peoplePhil Knight(Chairman Emeritus)Mark Parker(Executive Chairman)John Donahoe"
-                 "(President and CEO)|ProductsAthletic footwear & apparelAthletic & recreational productsSports "
-                 "equipment|Revenue US$37.40 billion (2020)|Operating income US$3.12 billion (2020)|"
-                 "Net income US$2.54 billion (2020)|Total assets US$31.34 billion (2020)|"
-                 "Total equity US$8.06 billion (2020)|Number of employees75,400 (2020)|Websitenike.com")
 
-parsed = {}
-cnum = 0
-str_pair = ["", ""]
-list_index = 0
+import socket
 
-for letter in info_to_parse:
+HOST_1 = ''  # Using the IP address specified in the instructions
+PORT_1 = 2113   # Using a port > 1023 as per HW instructions
+HOST_2 = 'bigballer420.pythonanywhere.com'
+PORT_2 = 80   # Port 80 is used for HTTP requests
 
-    if len(str_pair[list_index]) == 0:
-        str_pair[list_index] += letter
-        cnum += 1
-        continue
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.bind((HOST_1, PORT_1))  # Sets up connection to specified host and port
+    # .encode() is used to convert str to byte. Got an error when .encode() was not used
+    server.listen(1)
 
-    if list_index == 0:
+    while True:
 
-        # If letter is capital and previous letter was lowercase:
-        if (64 < ord(letter) < 91) and (96 < ord(str_pair[0][len(str_pair[0]) - 1]) < 123):
-            list_index += 1
+        connection, address = server.accept()  # Create a buffer of 1024 bytes to hold the response from GET request
+        request = connection.recv(1024).decode()
 
-        # If letter is "$" and previous letter was not a "|" or a space.
-        if ord(letter) == 36:
-            list_index += 1
+        # for company in sport_list:
+        info_to_parse = ""
 
-    if ord(letter) == 124:
-        key = str_pair[0]
-        value = str_pair[1]
-        parsed[key] = value
-        list_index = 0
-        str_pair = ["", ""]
-        continue
+        string = 'GET /?name=' + request + ' HTTP/1.1\r\nHost:bigballer420.pythonanywhere.com\r\n\r\n'
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as link:
+            link.connect((HOST_2, PORT_2))  # Sets up connection to specified host and port
+            # .encode() is used to convert str to byte. Got an error when .encode() was not used
+            link.send(string.encode())
 
-    str_pair[list_index] += letter
-    cnum += 1
+            response = link.recv(1024)  # Create a buffer of 1024 bytes to hold the response from GET request
 
-economic_data = {'Revenue': parsed['Revenue US'], 'Operating Income': parsed['Operating income US'],
-                 'Net Income': parsed['Net income US']}
+            info_to_parse += response.decode()  # Append response to info_to_parse
 
-print(economic_data)
+        print(info_to_parse)
+        print(request)
+        parsed = {}
+        rev = ""
+        op_in = ""
+        net_in = ""
+        go = True
 
-for key, value in economic_data.items():
-    value = value.replace(" (2020)", "")
-    value = value.replace("$", "")
-    value = value.replace(".", "")
-    value = value.replace(" billion", "0000000")
-    value = value.replace(" million", "0000")
-    value = int(value)
-    economic_data[key] = value
+        index = info_to_parse.find("Revenue")
+        index += len("Revenue")
 
-print(economic_data)
+        while go is True:
 
-pie_data = [economic_data["Revenue"] - economic_data['Operating Income'], economic_data['Operating Income'] -
-            economic_data['Net Income'], economic_data['Net Income']]
-pie_labels = ["Operating Costs", "Other Expenses", "Net Income"]
+            # 92 is "\". Undefined ascii characters show up in the form \uXXXX.
+            # These are things like the symbols for yen and pound sterling.
+            # To skip these, add 6 to index whenever "\" is found
+            if ord(info_to_parse[index]) == 92:
+                index += 6
+                continue
 
-fig1, ax1 = plt.subplots()
-ax1.pie(pie_data, labels=pie_labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+            # If a character is a number or '.', add it to rev
+            if 47 < ord(info_to_parse[index]) < 58 or ord(info_to_parse[index]) == 45\
+                    or ord(info_to_parse[index]) == 46:
+                rev += info_to_parse[index]
 
-plt.show()
+            # If a character is m or b, it signifies the end of relevant numbers. m for million, b for billion.
+            # Add letter to the string, then end loop by setting go to False
+            if info_to_parse[index] == 'm' or info_to_parse[index] == 'M' \
+                    or info_to_parse[index] == 'b' or info_to_parse[index] == 'B':
+                rev += info_to_parse[index]
+                parsed["Revenue"] = rev     # Create dictionary entry with the finished string "rev"
+                go = False
+
+            index += 1      # Increment index for next iteration of loop
+
+        index = info_to_parse.find("Operating income")
+        index += len("Operating income")
+
+        while go is False:
+
+            if ord(info_to_parse[index]) == 92:
+                index += 6
+                continue
+
+            if 47 < ord(info_to_parse[index]) < 58 or ord(info_to_parse[index]) == 45 \
+                    or ord(info_to_parse[index]) == 46:
+                op_in += info_to_parse[index]
+
+            if info_to_parse[index] == 'm' or info_to_parse[index] == 'M' \
+                    or info_to_parse[index] == 'b' or info_to_parse[index] == 'B':
+                op_in += info_to_parse[index]
+                parsed["Operating Income"] = op_in     # Create dictionary entry with the finished string "op_in"
+                go = True
+
+            index += 1
+
+        index = info_to_parse.find("Net income")
+        index += len("Net income")
+
+        while go is True:
+
+            if ord(info_to_parse[index]) == 92:
+                index += 6
+                continue
+
+            if 47 < ord(info_to_parse[index]) < 58 or ord(info_to_parse[index]) == 45 \
+                    or ord(info_to_parse[index]) == 46:
+                net_in += info_to_parse[index]
+
+            if info_to_parse[index] == 'm' or info_to_parse[index] == 'M' \
+                    or info_to_parse[index] == 'b' or info_to_parse[index] == 'B' or info_to_parse[index] == "(":
+                net_in += info_to_parse[index]
+                parsed["Net Income"] = net_in  # Create dictionary entry with the finished string "op_in"
+                go = False
+
+            index += 1
+
+        for key, value in parsed.items():
+
+            dec = value.find(".")
+            if dec == -1:
+                dec = len(value)-2
+            zeros = 0
+
+            if 'm' in value or 'M' in value:
+                zeros = 6
+                value = value.replace('m', '')
+                value = value.replace('M', '')
+            elif 'b' in value:
+                zeros = 9
+                value = value.replace('b', '')
+
+            value = value.replace('(', '')
+
+            while (len(value)-1) - dec < zeros:
+                value += '0'
+
+            value = value.replace('.', '')
+            value = int(value)
+            parsed[key] = value
+
+        if request == 'asics':
+            parsed['Net Income'] = 0
+        pie_data = [parsed["Revenue"] - parsed['Operating Income'],
+                    parsed['Operating Income'] - parsed['Net Income'], parsed['Net Income']]
+        pie_labels = ["Operating Costs", "Other Expenses", "Net Income"]
+
+        connection.send(str(pie_data).encode())
+        connection.send(str(pie_labels).encode())
+
+        connection.close()
--- store_game.py
+++ store_game.py
@@ -1,28 +1,120 @@
 from tkinter import *
+from tkinter.messagebox import askyesno
 import random
+import get_test     # Used to pull from webscraper
+
+# The following pulls data from the scrapper once when the game is started.
+stores = ['adidas', 'asics', 'athleta', 'callaway', 'canterbury', 'lululemon athletica', 'nike', 'puma',
+          'sondico', 'under armour']
+count = 1
+store_data = {}
+rank_string = ''
+for store in stores:
+    net_income_percent = round((get_test.get_info(store)[0][2]/sum(get_test.get_info(store)[0]))*100, 1)
+    store_data[store] = net_income_percent
+
+store_data = sorted(store_data.items(), key=lambda x: x[1], reverse=True)
+
+for store in store_data:
+    rank_string += str(count) + ". " + store[0] + ': ' + str(store[1]) + "%\n"
+    count += 1
+#   #   #   #
+
+MIN_ASSOCIATES = 3
+
+window_1 = Tk()  # Tk is a class imported from tkinter that creates a blank window
+window_1.title("Store Game")
+window_1.geometry("1200x800")
+
+global_actions_label = Label()
+notice_label = Label(window_1, text='')
 
 
 class Game:
     """The Game"""
 
     def __init__(self):
-        self.stores = []
+        self.stores = {}
         self.year = 1
         self.global_actions = 10
+
+    def open_store(self):
+        """"""
+        if store_game.global_actions >= 5:
+            store = Store()
+            store.setup()
+            store.id = len(self.stores) + 1
+            self.stores[store.id] = store
+            create_store_button(store.id)
+        else:
+            notice_label.config(text="NOTICE: Not enough actions. Minimum: 5 global actions.")
 
 
 class Store:
     """Store"""
 
     def __init__(self):
-        self.employees = []
+        self.employees = {}
         self.productivity = 0
+        self.id = 0
+        self.local_actions = 0
+        self.evaluations = False
 
     def hire_employee(self, position):
+        """Add an Employee object to the employees dictionary. id is the key and employee object is the value.
+        Note: employee id is not globally unique, but it is unique per store."""
+        if store_game.global_actions > 0 or self.local_actions > 0:
+            employee = Employee()
+            employee.setup(position)
+            employee.id = len(self.employees) + 1
+            self.employees[employee.id] = employee
+            self.calc_productivity()
+
+            if self.local_actions > 0:
+                self.local_actions -= 1
+            else:
+                store_game.global_actions -= 1
+                global_actions_label.config(text="Global actions remaining: " + str(store_game.global_actions))
+        else:
+            notice_label.config(text="NOTICE: No more actions remaining.")
+
+    def calc_productivity(self):
+        """Update store productivity. It is a sum of all the employee productivities in the store."""
+        productivity = 0
+        for eid, employee in self.employees.items():
+            productivity += employee.productivity
+        self.productivity = round(productivity, 2)
+
+    def setup(self):
+        """Setup a new store. It must start with three Sales Associates and a Jr. Manager."""
+
+        for i in range(0, MIN_ASSOCIATES):
+            self.hire_employee("Sales Associate")
+        self.hire_employee("Jr Manager")
+        store_game.global_actions -= 1
+        global_actions_label.config(text="Global actions remaining: " + str(store_game.global_actions))
+        self.local_actions += 1
+
+    def run_evaluations(self, store_win, store_id):
         """"""
-        employee = Employee()
-        employee.setup(position)
-        self.employees.append(employee)
+        response = open_confirm_win("Are you sure you want to run employee evaluations? Doing so will use one action.")
+
+        if response is True:
+            if (store_game.global_actions > 0 or self.local_actions > 0) and self.evaluations is False:
+                self.evaluations = True
+                if self.local_actions > 0:
+                    self.local_actions -= 1
+                else:
+                    store_game.global_actions -= 1
+                    global_actions_label.config(text="Global actions remaining: " + str(store_game.global_actions))
+                store_win.destroy()
+                open_store_win(store_id)
+            elif self.evaluations is True:
+                notice_label.config(text="NOTICE: Already ran employee evaluations for this store.")
+            else:
+                notice_label.config(text="NOTICE: No more actions remaining")
+        else:
+            return
 
 
 class Employee:
@@ -35,24 +127,20 @@ class Employee:
         self.productivity = 0
         self.leadership = None
         self.salary = 0
+        self.pos_db = {"Sales Associate": {"Leadership": False, "Salary": 30000},
+                       "Jr Manager": {"Leadership": True, "Salary": 45000},
+                       "Sr Manager": {"Leadership": True, "Salary": 60000}}
 
     def setup(self, position):
         """"""
-        info = self.position_database(position)
+        info = self.pos_db[position]
 
         self.position = position
-        self.productivity = round((random.random() + 1)/2, 3)
+        self.productivity = round((random.random() + 1)/2, 2)
         self.salary = info["Salary"]
 
         if info["Leadership"]:
-            self.leadership = round((random.random() + 1)/2, 3)
-
-    def position_database(self, position):
-        pos_db = {"Sales Associate": {"Leadership": False, "Salary": 30000},
-                  "Jr Manager": {"Leadership": True, "Salary": 45000},
-                  "Sr Manager": {"Leadership": True, "Salary": 60000}}
-
-        return pos_db[position]
+            self.leadership = round((random.random() + 1)/2, 2)
 
 
 def open_descript_win(position):
@@ -102,39 +190,79 @@ def open_rank_win():
     rank_win.geometry("800x600")
     rank_win.title("View Rank")
 
-    rank_example = Label(rank_win, text="Company            Percent Net Profit"
-                                        "\n1. Nike              6.8%"
-                                        "\n2. Addias            6.3%"
-                                        "\n3. My Company        5.9%"
-                                        "\n4. Reebok            5.7%"
-                                        "\n.                    ."
-                                        "\n.                    ."
-                                        "\n.                    .")
+    # rank_string data comes from scraper an it calculated during startup
+    rank_example = Label(rank_win, text=rank_string)
 
     rank_example.pack()
 
 
-window_1 = Tk()  # Tk is a class imported from tkinter that creates a blank window
-window_1.title("Store Game")
-window_1.geometry("1200x800")
+def create_store_button(sid):
+    store_button = Button(window_1, text="Store " + str(sid), command=lambda: open_store_win(sid))
+    store_button.pack()
 
 
-def main():
+def open_store_win(store_id):
+    store_win = Toplevel(window_1)
+    store_win.geometry("800x600")
+    store_win.title("Store " + str(store_id))
+    store = store_game.stores[store_id]
+    employees = store.employees
+    employee_info = ""
+    for eid, employee in employees.items():
+        employee_info += "Name: " + employee.name
+        employee_info += "  Position: " + employee.position + "\n"
+        if store.evaluations:
+            employee_info += "      Productivity: " + str(employee.productivity) + "\n"
+    store_info = Label(store_win, text="Store Productivity " + str(store.productivity) + "\n"
+                       + employee_info)
+    action_label = Label(store_win, text="Local actions remaining: " + str(store.local_actions))
+    run_evals_button = Button(store_win, text="Run Employee Evaluations",
+                              command=lambda: store.run_evaluations(store_win, store_id))
+    store_info.pack()
+    run_evals_button.pack(side=BOTTOM)
+    action_label.pack(side=BOTTOM)
 
-    store_game = Game()
 
-    rules_button = Button(window_1, text="View Basic Rules")
-    employee_button = Button(window_1, text="View Employee Descriptions", command=open_employee_win)
-    rank_button = Button(window_1, text="View Rank", command=open_rank_win)
-    location_button = Button(window_1, text="Open a New Location")
-
-    rules_button.pack(side=BOTTOM)
-    employee_button.pack(side=BOTTOM)
-    rank_button.pack(side=LEFT)
-    location_button.pack(side=TOP)
-
-    window_1.mainloop()
+def open_confirm_win(message):
+    response = askyesno(title="Confirmation", message=message)
 
 
-if __name__ == "__main__":
-    main()
+    # wait = IntVar()
+    #
+    # confirm_win = Toplevel(window_1)
+    # confirm_win.geometry("300x200")
+    # confirm_win.title("Confirmation")
+    # confirm_mess = Label(confirm_win, text=message)
+    # confirm_mess.pack()
+    #
+    # no = Button(confirm_win, text="No", command=confirm_win.destroy)
+    # yes = Button(confirm_win, text="Yes", command=lambda: wait.set(1))
+    # no.pack(side=BOTTOM)
+    # yes.pack(side=BOTTOM)
+    #
+    # yes.wait_variable(wait)
+    return response
+
+
+store_game = Game()
+
+rules_button = Button(window_1, text="View Basic Rules")
+employee_button = Button(window_1, text="View Employee Descriptions", command=open_employee_win)
+rank_button = Button(window_1, text="View Rank", command=open_rank_win)
+location_button = Button(window_1, text="Open a New Location", command=store_game.open_store)
+global_actions_label = Label(window_1, text="Global actions remaining: " + str(store_game.global_actions))
+
+rules_button.pack(side=BOTTOM)
+employee_button.pack(side=BOTTOM)
+global_actions_label.pack(side=BOTTOM)
+rank_button.pack(side=LEFT)
+location_button.pack(side=TOP)
+notice_label.pack(side=BOTTOM)
+
+store_game.open_store()
+
+window_1.mainloop()
+
+
+# if __name__ == "__main__":
+#     main()
